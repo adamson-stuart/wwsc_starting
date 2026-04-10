9e20ff4e6cb3d0612c5f23f72384a029676bf072
Stuart Adamson <stuart.adamson@gmail.com>
2026-04-11 00:18:04 +0100

    Adding

---
+++ RPi_DUMMY/GPIO.py
@@ -0,0 +1,28 @@
+# Dummy RPi.GPIO library for testing
+
+BOARD = 1
+HIGH=1
+OUT = 1
+IN = 1
+BCM=1
+HI=1
+LOW=1
+PUD_DOWN=1
+
+def input(a):
+   pass
+
+def setmode(a):
+   pass
+
+def setup(a, b, c=None):
+   pass
+
+def output(a, b):
+   pass
+
+def cleanup():
+   pass
+
+def setwarnings(flag):
+   pass
